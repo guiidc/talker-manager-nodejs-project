6cad361f8b0d168f2b58d99c95987fac04c65f78
Guilherme da Costa <guilhermedc93@gmail.com>
2021-10-26 15:53:08 -0300

    requisito 2 completo

--- Routers/talkerRouter.js
+++ Routers/talkerRouter.js
@@ -3,8 +3,20 @@ const fs = require('fs').promises;
 
 const talkersList = './talker.json';
 
+// requisito 1
 router.get('/', (_req, res) => {
   fs.readFile(talkersList)
   .then((talkers) => res.status(200).json(JSON.parse(talkers)));
 });
+
+// requisito 2
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  let list = await fs.readFile(talkersList);
+  list = JSON.parse(list);
+  const actualTalker = list.find((talker) => talker.id === Number(id));
+  if (actualTalker) return res.status(200).json(actualTalker);
+  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 module.exports = router;
